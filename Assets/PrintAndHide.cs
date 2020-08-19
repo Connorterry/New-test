511f997de4ac8d0161af43cd5d5276732db9cbdc
ConnorTerry <conna.terry@gmail.com>
2020-08-19 19:15:49 +1000

    active and renderer disabled

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -6,18 +6,27 @@ public class PrintAndHide : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    int j = 0;
     int i = 0;
     public Renderer rend;
+    
     void Start()
     {
-        
+       i = Random.Range(200,251);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        Debug.Log(gameObject.name + ": " +  i);
-        i++;
+        if(gameObject.tag=="Red"&&j==100){
+            gameObject.SetActive(false);
+        }
+        if(gameObject.tag=="Blue"&&j==i){
+            rend.enabled=false;
+
+        }
+        Debug.Log(gameObject.name + ": " +  j);
+        j++;
     }
 }
